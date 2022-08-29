c03103d98ce6aaa5659fb6fc222f7ec22f176cf2
fgulfocorrea <flaviogulfo@gmail.com>
2022-08-28 23:17:51 -0500

    se modifica la entidad mercancia

--- src/main/java/com/fgulfodev/invautomotriz/entity/Mercancia.java
+++ src/main/java/com/fgulfodev/invautomotriz/entity/Mercancia.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -28,11 +27,9 @@ public class Mercancia implements Serializable {
     private Date fechaModificacion;
 
     @ManyToOne
-    @JoinColumn(name = "usuario_id")
     private Usuario usuario;
 
     @ManyToOne
-    @JoinColumn(name = "usuario_mod_id")
     private Usuario usuarioModificacion;
 
     public Mercancia() {
